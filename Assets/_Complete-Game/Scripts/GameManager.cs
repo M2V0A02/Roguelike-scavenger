91d97519559a7584a0d2f346a135fc083c94c5f6
M2V0A02 <volodin.mixa2015@yandex.ru>
2022-05-31 00:27:38 +0500

    PlayerScript

--- Assets/_Complete-Game/Scripts/GameManager.cs
+++ Assets/_Complete-Game/Scripts/GameManager.cs
@@ -7,8 +7,10 @@ public class GameManager : MonoBehaviour
     
     public static GameManager instance = null;
     public BoardManager boardScript;
-    private int level = 3;
+    public int playerFoodPoints = 100;
+    [HideInInspector] public bool playersTurn = true;
 
+    private int level = 3;
 
     void Awake ()
     {
@@ -24,6 +26,11 @@ public class GameManager : MonoBehaviour
     void InitGame()
     {
         boardScript.SetupScene(level);
+    }
+
+    public void GameOver()
+    {
+        enabled = false;
     }
 
     void Start()
