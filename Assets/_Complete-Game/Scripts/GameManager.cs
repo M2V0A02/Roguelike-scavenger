72847621542d8b7bc04dade8239e941880a253c2
M2V0A02 <volodin.mixa2015@yandex.ru>
2022-05-30 20:35:55 +0500

    Создал GameManager

--- Assets/_Complete-Game/Scripts/GameManager.cs
+++ Assets/_Complete-Game/Scripts/GameManager.cs
@@ -4,7 +4,28 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+    public static GameManager instance = null;
+    public BoardManager boardScript;
+    private int level = 3;
+
+
+    void Awake ()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
+    }
+
     void Start()
     {
         
